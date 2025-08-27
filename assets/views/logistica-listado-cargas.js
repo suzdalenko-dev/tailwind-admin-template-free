ab759b9fa246f3b322fe8cf0686723b758fe0767
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2025-08-27 14:33:16 +0200

    suzdalenko

--- assets/views/logistica-listado-cargas.js
+++ assets/views/logistica-listado-cargas.js
@@ -17,12 +17,12 @@ function geRefreshGemaRoutes(){
             let listLoads        = r.data;
             listLoads.map(x => {
                 htmlLoadContent += `<tr>
-                                        <td class="border px-2 py-1 text-center"><a href="#logistica-listado-viajes?load_id=${x.load_id}&load_date=${x.load_date}"><b class="color_blue">${x.load_id}</b></a></td>
-                                        <td class="border px-2 py-1 text-center"></td>
-                                        <td class="border px-2 py-1 text-center">${x.load_week}</td>
-                                        <td class="border px-2 py-1 text-center">${(formatDateToEuropean(x.load_date))}</td>
-                                        <td class="border px-2 py-1 text-center"></td>
-                                        <td class="border px-2 py-1 text-center"></td>
+                                        <td class="border expanded px-2 py-1 text-center"><a href="#logistica-listado-viajes?load_id=${x.load_id}&load_date=${x.load_date}"><b class="color_blue">${x.load_id}</b></a></td>
+                                        <td class="border expanded px-2 py-1 text-center"></td>
+                                        <td class="border expanded px-2 py-1 text-center">${x.load_week}</td>
+                                        <td class="border expanded px-2 py-1 text-center">${(formatDateToEuropean(x.load_date))}</td>
+                                        <td class="border expanded px-2 py-1 text-center"></td>
+                                        <td class="border expanded px-2 py-1 text-center"></td>
                                     </tr>`;
                 if(x && x.travel_names && x.travel_names.length > 0){
                     let colorGreenLine = '';
@@ -32,12 +32,12 @@ function geRefreshGemaRoutes(){
                             colorGreenLine = `class="class_green" style="color:white;"`;
                         }
                         htmlLoadContent += `<tr ${colorGreenLine}>
-                                                <td class="border px-2 py-1 text-center"></td>
-                                                <td class="border px-2 py-1 text-left"><a href="#logistica-viaje-detalle?load_id=${x.load_id}&truck_id=${y.truck_id}"><b class="color_blue" ${colorGreenLine}>${y.truck_id}. ${y.truck_name}</b></a></td>
-                                                <td class="border px-2 py-1 text-center"></td>
-                                                <td class="border px-2 py-1 text-center"></td>
-                                                <td class="border px-2 py-1 text-center">${y.palets}</td>
-                                                <td class="border px-2 py-1 text-center">${y.sum_input_pal}</td>
+                                                <td class="border expanded px-2 py-1 text-center"></td>
+                                                <td class="border expanded px-2 py-1 text-left"><a href="#logistica-viaje-detalle?load_id=${x.load_id}&truck_id=${y.truck_id}"><b class="color_blue" ${colorGreenLine}>${y.truck_id}. ${y.truck_name}</b></a></td>
+                                                <td class="border expanded px-2 py-1 text-center"></td>
+                                                <td class="border expanded px-2 py-1 text-center"></td>
+                                                <td class="border expanded px-2 py-1 text-center">${y.palets}</td>
+                                                <td class="border expanded px-2 py-1 text-center">${y.sum_input_pal}</td>
                                             </tr>`;
                     });
                 }
