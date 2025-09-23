ac785e2f5f9d8c075b949c5d0c9416c8870378f3
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2025-09-23 16:14:57 +0200

    m

--- assets/views/logistica-detalle-carga-regional.js
+++ assets/views/logistica-detalle-carga-regional.js
@@ -1,5 +1,6 @@
 let yearLDCR;
 let load_idLDCR;
+let colorGreenLDCR = ''
 
 function logisticaDetalleCargaRegionalInit(){
     document.title = 'Carga Regional Detalle'
@@ -13,30 +14,36 @@ function logisticaDetalleCargaRegionalInit(){
 }
 
 function getLDCR(){
-    document.getElementById('tableLDCR').innerHTML = 'Cargando..';
-
-    suzdalenkoGet(`/logistica/get/${yearLDCR}/${load_idLDCR}/custom_carga_serie/`, (res) => {
+    suzdalenkoGet(`/logistica/get/${yearLDCR}/${load_idLDCR}/get_refresh_custom_load_02/`, (res) => {
         let html = '';
         
-        console.log(res.data.res)
+        if(res && res.data && res.data.lines && res.data.lines.length > 0){
+            res.data.lines.map(x => {
+                colorGreenLDCR = '';
+                if(x.clicked == 1){
+                    colorGreenLDCR = 'class="class_green" style="color:white;"'
+                }
 
-        if(res && res.data && res.data.res && res.data.res.length > 0){
-            res.data.res.map(x => {
-                console.log(x)
-
-                html += `<tr>
-                    <td class="border expanded px-2 py-1 text-center">${x.NUMERO_PROPUESTA}</td>
-                    <td class="border expanded px-2 py-1 text-left">${x.DESCRIPCION_ARTICULO} ${x.ARTICULO}</td>
-                    <td class="border expanded px-2 py-1 text-center">${x.PREPARADO > 0 ? x.PREPARADO : ''}</td>
-                    <td class="border expanded px-2 py-1 text-center">${x.PEDIDO > 0 ? x.PEDIDO: ''}</td>
+                html += `<tr ${colorGreenLDCR}>
+                    <td class="border expanded px-2 py-1 text-center">${formatDateToEuropean(x.load_date)}</td>
+                    <td class="border expanded px-2 py-1 text-center">${x.load_id}</td>
+                    <td class="border expanded px-2 py-1 text-left" onclick="orderClickedLDCR(${x.ejercicio}, '${x.load_id}', '${x.article_code}')"><span class="hovered">${x.article_name} ${x.article_code}</span></td>
+                    <td class="border expanded px-2 py-1 text-center">${x.preparado > 0 ? x.preparado : ''}</td>
+                    <td class="border expanded px-2 py-1 text-center">${x.pedido > 0 ? x.pedido: ''}</td>
                     
-                    <td class="border expanded px-2 py-1 text-center">${x.UND_PED > 0 ? x.UND_PED : ''}</td>
-                    <td class="border expanded px-2 py-1 text-center">${x.CAJAS_PED > 0 ? x.CAJAS_PED : ''}</td>
+                    <td class="border expanded px-2 py-1 text-center">${x.und_ped > 0 ? x.und_ped : ''}</td>
+                    <td class="border expanded px-2 py-1 text-center">${x.cajas_ped > 0 ? x.cajas_ped : ''}</td>
                 </tr>`;
             });
             document.getElementById('tableLDCR').innerHTML = html;
         } else {
             document.getElementById('tableLDCR').innerHTML = 'No hay datos..';
         }
+    });
+}
+
+function orderClickedLDCR(ejercicio, load_id, article_code){
+    suzdalenkoGet(`logistica/action/0/0/regional_clicked/?ejercicio=${ejercicio}&load_id=${load_id}&article_code=${article_code}`, (res) => {
+        getLDCR();
     });
 }
--- assets/views/logistica-listado-cargas-regional.js
+++ assets/views/logistica-listado-cargas-regional.js
@@ -1,3 +1,5 @@
+let colorGreenLDCR2 = ''
+
 function logisticaListadoCargasRegionalInit(){
     document.title = 'Cargas en Regional'
     document.getElementById('slugTitle').innerHTML = '';
@@ -8,20 +10,36 @@ function logisticaListadoCargasRegionalInit(){
 function getLLCR(){
     document.getElementById('tableLLCR').innerHTML = 'Cargando..';
 
-    suzdalenkoGet('logistica/get/0/0/hojas_carga_serie_02/', (res) => {
+    suzdalenkoGet('logistica/get/0/0/get_refresh_head_02/', (res) => {  
         let html = '';
         
-        console.log(res.data.res)
-
         if(res && res.data && res.data.res && res.data.res.length > 0){
-            res.data.res.map(x => {
-                console.log(x)
 
+            res.data.res.map(x => {
                 html += `<tr>
-                    <td class="border expanded px-2 py-1 text-center"><a href="#logistica-detalle-carga-regional?year=${x.EJERCICIO}&load_id=${x.NUMERO_PROPUESTA}"><b class="color_blue">${((x.FECHA_CARGA))}</b></a></td>    
-                    <td class="border expanded px-2 py-1 text-center"><a href="#logistica-detalle-carga-regional?year=${x.EJERCICIO}&load_id=${x.NUMERO_PROPUESTA}"><b class="color_blue">${x.NUMERO_PROPUESTA}</b></a></td>
-                    <td class="border expanded px-2 py-1 text-center">${x.CONDUCTOR} ${x.MATRICULA}</td>
+                    <td class="border expanded px-2 py-1 text-center">${formatDateToEuropean(x.date)}</td>    
+                    <td class="border expanded px-2 py-1 text-center"></td>
+                    <td class="border expanded px-2 py-1 text-center"></td>
                 </tr>`;
+
+                if(x.loads && x.loads.length > 0){
+                    x.loads.map(y => {
+
+                        colorGreenLDCR2 = '';
+                        if(y.clicked == 1){
+                            colorGreenLDCR2 = 'class="class_green" style="color:white;"'
+                        }
+
+
+                        html += `<tr ${colorGreenLDCR2}>
+                                    <td class="border expanded px-2 py-1 text-center"></td>    
+                                    <td class="border expanded px-2 py-1 text-center"><a href="#logistica-detalle-carga-regional?year=${y.ejercicio}&load_id=${y.load_id}"><b class="color_blue" ${colorGreenLDCR2}>${y.load_id}</b></a></td>
+                                    <td class="border expanded px-2 py-1 text-center">${y.conductor == 'None' ? '': y.conductor} ${y.matricula}</td>
+                                </tr>`;
+                    });
+                }
+
+    ;
             });
             document.getElementById('tableLLCR').innerHTML = html;
         } else {
