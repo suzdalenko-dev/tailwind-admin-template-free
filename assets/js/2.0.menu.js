9e875cecfb4ffbfb1734118a66acef545d743132
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2025-11-13 15:46:23 +0100

    suzdalenko

--- assets/js/2.0.menu.js
+++ assets/js/2.0.menu.js
@@ -40,8 +40,8 @@ let menuData = [
         { title: 'Inventario', icon: '', submenu: null , view: 'power-bi?name=ventas-inventario', roles: ['ventas;ventas20;'] },
         { title: 'Compraración importes', icon: '', submenu: null , view: 'power-bi?name=compraracion-de-importes', roles: ['ventas;ventas20;'] },
      ]},
-     { title: 'Informes', icon: '', roles: ['propietario;'], submenu: [
-        { title: 'Ventas ORG. Comercial', icon: '', submenu: null , view: 'power-bi?name=venta-contabilidad-excel-libra', roles: ['propietario;'] },
+     { title: 'Informes', icon: '', roles: ['propietario;finanzas;'], submenu: [
+        { title: 'Ventas ORG. Comercial', icon: '', submenu: null , view: 'power-bi?name=venta-contabilidad-excel-libra', roles: ['propietario;finanzas;'] },
      ]},
   ]},
 ];
