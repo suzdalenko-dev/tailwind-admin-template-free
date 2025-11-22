d59c3b6567b23b75aae201a94ded1db72a3c54f7
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2025-11-22 21:13:23 +0100

    suzdalenko

--- assets/views/power-bi.js
+++ assets/views/power-bi.js
@@ -2,18 +2,22 @@ function powerBiInit(){
     document.getElementById('slugTitle').innerHTML = '';
     let powerBiName = parseHashRoute();
     powerBiName     = powerBiName.params.name || 'None';
-    document.title = powerBiName;
+    document.title  = powerBiName;
 
     getReportPBI(powerBiName);
 }
 
-function getReportPBI(powerBiName){
+function getReportPBI(powerBiName){ console.log(window.localStorage)
     let userName    = window.localStorage.getItem('username') || 'None';
-    let password = window.localStorage.getItem('password') || 'None';
+    let password    = window.localStorage.getItem('password') || 'None';
+    let userId      = window.localStorage.getItem('user_id') || 'None';
+    let userRole    = window.localStorage.getItem('role') || 'None';;
     let formData = new FormData();
         formData.append('report_name', powerBiName);
         formData.append('user_name', userName);
         formData.append('password', password);
+        formData.append('user_id', userId);
+        formData.append('user_role', userRole);
 
     fetch(HTTP_HOST+'powerbi/get/0/0/get_custom_report/', {method:'POST', body: formData}).then(r => r.json()).then(r => {
         if(r && r.data && r.data.res){
