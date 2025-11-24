c4222f743c5f34fdd68cdb1cb2adf3a1dd422607
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2025-11-24 14:47:19 +0100

    su

--- assets/views/logistica-cierre-hojas-carga.js
+++ assets/views/logistica-cierre-hojas-carga.js
@@ -1,5 +1,6 @@
 let fechaDesdeLCHC;
 let fechaHastaLCHC;
+let serieLCHC = 'all';
 
 function logisticaCierreHojasCargaInit(){
     document.title = 'Repaso de cierre de hojas de carga';
@@ -22,8 +23,7 @@ function initLCHC(){
     document.getElementById('inputToLCHC').value   = fechaHastaLCHC;
     document.getElementById('tableLCJC').innerHTML = '<br>Cargando datos..';
 
-    fetch(HTTP_HOST+'logistica/get/0/0/repaso_hc_sin_cerrar/?date_from='+fechaDesdeLCHC+'&date_to='+fechaHastaLCHC).then(r => r.json()).then(r => {
-        console.log(r)
+    fetch(HTTP_HOST+'logistica/get/0/0/repaso_hc_sin_cerrar/?date_from='+fechaDesdeLCHC+'&date_to='+fechaHastaLCHC+'&serie='+serieLCHC).then(r => r.json()).then(r => {
         if(r && r.data && r.data.out && r.data.out.length){
             let html = '';
             r.data.out.map(x => {
@@ -34,7 +34,7 @@ function initLCHC(){
                         <td class="border px-2 py-1 text-center"></td><td class="border px-2 py-1 text-center"></td><td class="border px-2 py-1 text-center"></td><td class="border px-2 py-1 text-center"></td><td class="border px-2 py-1 text-center"></td>
                     </tr>`;
                     if(x.articulos){
-                        x.articulos.map(a => { console.log(a)
+                        x.articulos.map(a => {
                             html += `<tr>
                                 <td class="border px-2 py-1 text-center"></td>
                                 <td class="border px-2 py-1 text-center"></td>
@@ -49,11 +49,32 @@ function initLCHC(){
                     }
             });
             document.getElementById('tableLCJC').innerHTML = html;
-            console.log(r.data.out)
         } else {
             document.getElementById('tableLCJC').innerHTML = '<br>No hay datos que mostrar.';
+        }
+        if(r && r.data && r.data.serie_hc){
+            let serieVal = '<option value="all" selected>Todas las series 01, 02, 03, 04 </option>';
+            console.log(r.data)
+            r.data.serie_hc.map(s => {
+                serieVal += `<option value="${s}">${s} ${serieNameLCHC(s)}</option>`;
+            });
         }
     }).catch(e => {
         showM(e, 'error');
     });
+}
+
+function changeSerieLCHC(val){
+    serieLCHC = val;
+    initLCHC();
+}
+
+function serieNameLCHC(x) {
+    switch (x) {
+        case '01': return 'NACIONAL';
+        case '02': return 'REGIONAL';
+        case '03': return 'CUBA';
+        case '04': return 'EXPORTACION';
+        default:   return '';  
+    }
 }
