99c64bbea041644ad1d53617398f8218b15f800b
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2026-01-21 10:50:34 +0100

    suzdalenko

--- assets/js/2.0.menu.js
+++ assets/js/2.0.menu.js
@@ -25,7 +25,7 @@ let menuData = [
     { title: 'Comparación Alm.98', icon: '🆚', submenu: null , view: 'almacen-importacion-vs-resto', roles: ['logistica;finanzas;'] },
     { title: 'Hoja de Contenedor', icon: '🧾', submenu: null , view: 'logistica-hoja-contenedor', roles: ['logistica;'] },
     { title: 'Cierre de HC', icon: '📄', submenu: null , view: 'logistica-cierre-hojas-carga', roles: ['logistica;'] },
-    { title: 'Análisis de Pedido', icon: '📊', submenu: null , view: 'logistica-analisis-pedido-completo-picking', roles: ['logistica;'] },
+    { title: 'Análisis de Pedido', icon: '📊', submenu: null , view: 'logistica-analisis-pedido-completo-picking', roles: ['logistica;produccion;produccion20;produccion30;'] },
   ]},
   { title: 'Compras', icon: '🛍️', roles: ['compras;calidad;produccion;compras20;ventas;ventas20;exportacion;produccion20;'], submenu: [
     { title: 'Llegadas Pendientes', icon: '🚢', submenu: null , view: 'compras-llegadas-contenedores', roles: ['compras;produccion;'] },
