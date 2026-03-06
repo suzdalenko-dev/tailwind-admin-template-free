4a7e01e1fe63e69d89af9b7a7d622eb4d0bfedd9
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2026-03-06 16:40:50 +0100

    suzdalenko

--- assets/js/2.0.menu.js
+++ assets/js/2.0.menu.js
@@ -22,9 +22,9 @@ let menuData = [
       { title: 'Gestión de operarios', icon: '', submenu: null , view: 'produccion-de-operarios', roles: ['produccion;produccion20;'] },
       { title: 'Informe horas OF', icon: '', submenu: null , view: 'produccion-informe-horas-operarios', roles: ['produccion;produccion20;'] },
     ]},
-    { title: 'Industria 4.0', icon: '🖥️', roles: ['mantenimiento;produccion;calidad;produccion30;'], submenu: [
-      { title: 'Ajustes', icon: '', submenu: null , view: 'produccion-industria-ajustes-articulo', roles: ['mantenimiento;produccion;calidad;produccion30;'] },
-      { title: 'Infome', icon: '', submenu: null , view: 'power-bi?name=industria-4.0.1', roles: ['mantenimiento;produccion;calidad;produccion30;'] },
+    { title: 'Industria 4.0', icon: '🖥️', roles: ['mantenimiento;produccion;calidad;produccion30;produccion20;'], submenu: [
+      { title: 'Ajustes', icon: '', submenu: null , view: 'produccion-industria-ajustes-articulo', roles: ['mantenimiento;produccion;calidad;produccion30;produccion20;'] },
+      { title: 'Infome', icon: '', submenu: null , view: 'power-bi?name=industria-4.0.1', roles: ['mantenimiento;produccion;calidad;produccion30;produccion20;'] },
     ]},
   ]},
   { title: 'Logística', icon: '🚚', roles: ['logistica;finanzas;'], submenu: [
